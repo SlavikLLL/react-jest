c2ebecf71260afe63494798a8a6d0ea0de9fb63b
SlavikLLL <listopads091@gmail.com>
2021-12-22 00:24:47 +0200

    some tests added

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -15,19 +15,35 @@
     //stack.push('one');
     //stack.push('two');
     //stack.pop();
-  //  expect(stack.pop()).toEqual('one');
+    //expect(stack.pop()).toEqual('one');
 //});
 
 import  arrayMethods from '../src/index.js';
-//const arr = [1,2,3,5,11,25];
+//indexOf
 test('find index',() =>{
-  const arr = [1,2,3,5,11,25];
+  
   const method1 = new arrayMethods();
+  const arr = [1,2,3,5,11,25];
   method1.indexof(arr,25);
   expect(method1.indexof(arr,25)).toEqual(5)
+});
+test('not empty',()=>{
+  const arr = [1,2,3,5,11,25];
+  expect(arr.length).not.toEqual(0);
+});
+test("index not founded",()=>{
+  const method1 = new arrayMethods();
+   const arr = [1,2,3,5,11,25];
+   method1.indexof(arr,15);
+   expect(method1.indexof(arr,15)).toEqual(-1);
+
+})
+//includes
+test("include",()=>{
+  const method2 = new arrayMethods();
+  const arr = [1,3,4,5,6,7];
+  method2.include(arr,5);
+  expect(method2.include(arr,5)).toEqual(true)
 })
 
-//import reverse from "../src/index.js";
-//test("1",()=>{
-  //expect(reverse('hello')).toEqual("olleh");
-//})
+
--- src/1.js
+++ src/1.js
@@ -1,1 +1,11 @@
-let promise = new Promise()
+function func(arr,elem){
+    for(let i = 0;i<=arr.length;i++){
+        if(arr[i] == elem){
+            return true
+        }
+    
+    }
+    return false
+    
+}
+const arr = [1,2,3,4,5];
